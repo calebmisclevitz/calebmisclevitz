67d99c149bc5e26474907ec5103f55862912516c
Caleb Misclevitz <calebmisclevitz@gmail.com>
2017-07-28 23:17:56 -0700

    font size on mobile test

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,18 +2,25 @@ $(document).ready(function() {
     
     console.log("ready");
 
+    offset = 0;
+    
     $(window).on('scroll', function(event) {
 
         scrolltop = $(window).scrollLeft();
         
-        console.log(scrolltop);
-
         scrolltop = Math.round(scrolltop / 20);
 
         image_num = scrolltop%12;
+        console.log(image_num);
+        
 
-        $('.piece').css('background-image', 'url("work/' + image_num + '.jpg")');   
+        $('.piece').css('background-image', 'url("work/' + image_num + '.jpg")');
 
+    });
+    
+    $(window).scroll(function() {
+        var scrolledY = $(window).scrollLeft();
+        $('.PIECE').css('background-position', 'top ' + ((scrolledY)) + 'px');
     });
     
 });
