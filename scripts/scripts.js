993fd8b829767bacd3a44a24c81557f6fc5ca7a2
Caleb Misclevitz <calebmisclevitz@gmail.com>
2017-07-28 17:47:31 -0700

    fixed scrolling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,19 @@
-$(window).on('scroll', function(event) {
-          
-    scrolltop = $(window).scrollLeft();
-     
-    scrolltop = Math.round(scrolltop / 100);
-     
-    image_num = scrolltop%10;
-         
-    $('.work').css('background-image', 'url("work/' + image_num + '.jpg")');     
+$(document).ready(function() {
+    
+    console.log("ready");
+
+    $(window).on('scroll', function(event) {
+
+        scrolltop = $(window).scrollLeft();
+        
+        console.log(scrolltop);
+
+        scrolltop = Math.round(scrolltop / 100);
+
+        image_num = scrolltop%10;
+
+        $('.work').css('background-image', 'url("work/' + image_num + '.jpg")');   
+
+    });
+    
 });
